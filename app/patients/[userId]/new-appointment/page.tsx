e56602ce74ed46b468b3ea57268fcd31e3521ab1
Sakthiganesan <sakthiganesan158@gmail.com>
2024-07-15 22:12:14 +0530

    going on

--- app/patients/[userId]/new-appointment/page.tsx
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -2,11 +2,11 @@ import AppointmentForm from "@/components/forms/AppointmentForm";
 import Image from "next/image";
 import React from "react";
 
-function AppointmentPage() {
+async function AppointmentPage({ params: { userId } }: SearchParamProps) {
   return (
     <div className="flex h-screen max-h-screen">
       <section className="remove-scrollbar container">
-        <div className="sub-container max-w-[860px] flex-1 flex-col py-10">
+        <div className="sub-container max-w-[860px] flex-1 justify-between">
           <Image
             src="/assets/icons/logo-full.svg"
             height={1000}
@@ -25,8 +25,8 @@ function AppointmentPage() {
         src="/assets/images/appointment-img.png"
         height={1000}
         width={1000}
-        alt="patient"
-        className="side-img max-w-[390px]"
+        alt="apointment"
+        className="side-img max-w-[390px] bg-bottom"
       />
     </div>
   );
